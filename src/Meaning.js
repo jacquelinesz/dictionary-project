d5812b9895f704d60ad8d41d6619809dc3a3fab2
jacquelinesz <zuritajackie@gmail.com>
2022-08-26 16:18:03 -0400

    Fixed synonym component

--- src/Meaning.js
+++ src/Meaning.js
@@ -15,7 +15,7 @@ export default function Meaning(props) {
               <strong>Example: </strong>
               <em>{definition.example}</em>
               <br />
-              <Synonyms synonyms={definition.synonyms} />
+              <Synonyms synonyms={props.meaning.synonyms} />
             </p>
           </div>
         );
