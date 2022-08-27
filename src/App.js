8fea09bf652c3dde748e5382525681ec263dd2ad
jacquelinesz <zuritajackie@gmail.com>
2022-08-27 13:30:28 -0400

    Added info to footer and minor css edits

--- src/App.js
+++ src/App.js
@@ -10,7 +10,31 @@ export default function App() {
           <Dictionary />
         </main>
         <footer className="App-footer">
-          <small>Coded by Jacqueline Solorzano</small>
+          This project was coded by{" "}
+          <a
+            href="https://transcendent-snickerdoodle-662c96.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Jacqueline Solorzano
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/jacquelinesz/dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://keen-dango-78f283.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+          .
         </footer>
       </div>
     </div>
